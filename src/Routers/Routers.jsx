3389f9084b6cc566a48eb2ca3696b4ccfba30f62
Sergey <S.prihodchenko@yandex.ru>
2022-12-28 14:43:22 +0300

    a

---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Routers = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Routers;
---
+++ src/Teamplates/AddProduct.jsx
@@ -0,0 +1,19 @@
+const AddProduct = () => {
+    return (
+        <div className="">
+            <form action="">
+                <input type="text" />
+                <input type="text" name="name" id="" />
+                <input type="text" name="count" id="" />
+                <div className="">
+                <label for="img_upload"></label>    
+                <input type="file" name="img_upload" id="" style={{display: 'none'}}/>
+                </div>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </form>
+            <form action="">
+
+            </form>
+        </div>
+    );
+}
